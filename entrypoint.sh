0d2c105399f791f1ad79416e53099eae6e797ccd
Darren Kuefner <darren.kuefner@gmail.com>
2021-06-23 08:53:01 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-echo "::set-output name=test-var::MyTestVar"
+repo_token=$1
+
+milestone="milestone"
+
+IFS='/' read owner repository <<< "$GITHUB_REPOSITORY"
+
+release_url=$(dotnet gitreleasemanager create \
+--milestone $milestone \
+--targetcommitish $GITHUB_SHA \
+--token $repo_token \
+--owner $owner \
+--repository $repository)
+
+echo "::set-output name=test-var::$release_url"
 
 exit 0
