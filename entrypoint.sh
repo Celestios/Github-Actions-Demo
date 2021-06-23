0ed9f243b5c254d44df2408b4b165866be409ff2
Darren Kuefner <darren.kuefner@gmail.com>
2021-06-23 09:27:24 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,9 @@ repo_token=$1
 
 echo "token: $repo_token"
 
-milestone="milestone1"
-
 IFS='/' read owner repository <<< "$GITHUB_REPOSITORY"
 
 release_url=$(dotnet gitreleasemanager create \
---milestone $milestone \
 --targetcommitish $GITHUB_SHA \
 --token $repo_token \
 --owner $owner \
